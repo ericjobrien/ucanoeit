10979f7406a8bd7c991742480bace4983549f711
Eric O'Brien <obrienjeric@gmail.com>
2021-12-04 06:24:13 -0500

    Changes

--- src/main/java/com/ucanoeit/capstone/controllers/ProductPageController.java
+++ src/main/java/com/ucanoeit/capstone/controllers/ProductPageController.java
@@ -1,14 +1,33 @@
 package com.ucanoeit.capstone.controllers;
 
+import com.ucanoeit.capstone.repositories.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductPageController {
 
-    @RequestMapping("/product-page")
-    public String getProductPage(Model model) {
+    private final ProductRepository productRepository;
+
+    public ProductPageController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @RequestMapping("/products")
+    public String getProducts(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+
+        return "product-page";
+    }
+
+    @RequestMapping("/product-page/{id}")
+    public String getProductPage(@PathVariable Long id, Model model) {
+
+
+        productRepository.findById(id).ifPresent(o -> model.addAttribute("product", o));
         return "product-page";
     }
 }
